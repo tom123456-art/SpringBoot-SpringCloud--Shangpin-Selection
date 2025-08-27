a67a61e29a2fffde910d4073f35c5e43f1e3baa8
tom123456-art <2470688428@qq.com>
2025-08-27 17:02:29 +0800

    实现商品管理中商品规格管理的增删改查功能

---
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/ProductSpecController.java
@@ -0,0 +1,45 @@
+package com.atguigu.spzx.manager.controller;
+
+import com.atguigu.spzx.manager.service.ProductSpecService;
+import com.atguigu.spzx.model.entity.product.ProductSpec;
+import com.atguigu.spzx.model.vo.common.Result;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value="/admin/product/productSpec")
+public class ProductSpecController {
+
+    @Autowired
+    private ProductSpecService productSpecService ;
+
+    //列表
+    @GetMapping("/{page}/{limit}")
+    public Result findByPage(@PathVariable Integer page, @PathVariable Integer limit) {
+        PageInfo<ProductSpec> pageInfo = productSpecService.findByPage(page, limit);
+        return Result.build(pageInfo , ResultCodeEnum.SUCCESS) ;
+    }
+
+    //添加
+    @PostMapping("save")
+    public Result save(@RequestBody ProductSpec productSpec) {
+        productSpecService.save(productSpec);
+        return Result.build(null , ResultCodeEnum.SUCCESS) ;
+    }
+
+    //修改
+    @PutMapping("updateById")
+    public Result updateById(@RequestBody ProductSpec productSpec) {
+        productSpecService.updateById(productSpec);
+        return Result.build(null , ResultCodeEnum.SUCCESS) ;
+    }
+
+    //删除
+    @DeleteMapping("/deleteById/{id}")
+    public Result deleteById(@PathVariable Long id) {
+        productSpecService.deleteById(id);
+        return Result.build(null , ResultCodeEnum.SUCCESS) ;
+    }
+}
---
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/mapper/ProductSpecMapper.java
@@ -0,0 +1,21 @@
+package com.atguigu.spzx.manager.mapper;
+
+import com.atguigu.spzx.model.entity.product.ProductSpec;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ProductSpecMapper {
+    //列表
+    List<ProductSpec> findByPage();
+
+    //添加
+    void save(ProductSpec productSpec);
+
+    //修改
+    void updateById(ProductSpec productSpec);
+
+    //删除
+    void deleteById(Long id);
+}

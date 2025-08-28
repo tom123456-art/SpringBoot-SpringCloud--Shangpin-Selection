31c19f3c9909f5a9855fdfee93b824b1df16bc98
tom123456-art <2470688428@qq.com>
2025-08-28 21:00:27 +0800

    微服务注册，搭建网关服务，添加redis缓存(RedisTemplate和SpringCache实现)

--- spzx-parent/spzx-service/service-product/src/main/java/com/atguigu/spzx/product/controller/CategoryController.java
+++ spzx-parent/spzx-service/service-product/src/main/java/com/atguigu/spzx/product/controller/CategoryController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Tag(name = "分类接口管理")
 @RestController
 @RequestMapping(value="/api/product/category")
-@CrossOrigin
+
 public class CategoryController {
    
    @Autowired
--- spzx-parent/spzx-service/service-product/src/main/java/com/atguigu/spzx/product/service/impl/CategoryServiceImpl.java
+++ spzx-parent/spzx-service/service-product/src/main/java/com/atguigu/spzx/product/service/impl/CategoryServiceImpl.java
@@ -1,13 +1,18 @@
 package com.atguigu.spzx.product.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.spzx.model.entity.product.Category;
 import com.atguigu.spzx.product.mapper.CategoryMapper;
 import com.atguigu.spzx.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 // 接口实现类
@@ -17,26 +22,42 @@ public class CategoryServiceImpl implements CategoryService {
 	@Autowired
 	private CategoryMapper categoryMapper;
 
-    @Override
-    public List<Category> selectOneCategory() {
-        return categoryMapper.selectOneCategory();
-    }
+    @Autowired
+    private RedisTemplate<String , String> redisTemplate ;
 
     @Override
+    public List<Category> selectOneCategory() {
+
+        // 从Redis缓存中查询所有的一级分类数据
+        String categoryOneJSON = redisTemplate.opsForValue().get("category:one");
+        if(!StringUtils.hasText(categoryOneJSON)) {
+            List<Category> existCategoryList = JSON.parseArray(categoryOneJSON, Category.class);
+//            log.info("从Redis缓存中查询到了所有的一级分类数据");
+            return existCategoryList;
+        }
+
+        List<Category> categoryList = categoryMapper.selectOneCategory();
+//        log.info("从数据库中查询到了所有的一级分类数据");
+        redisTemplate.opsForValue().set("category:one" , JSON.toJSONString(categoryList) , 7 , TimeUnit.DAYS);
+        return categoryList ;
+    }
+
+    @Cacheable(value = "category" , key = "'all'")
+    @Override
     public List<Category> findCategoryTree() {
-        List<Category> categoryList = categoryMapper.findAll();
+        List<Category> allCategoryList = categoryMapper.findAll();
         //全部一级分类
-        List<Category> oneCategoryList = categoryList.stream().filter(item ->
+        List<Category> oneCategoryList = allCategoryList.stream().filter(item ->
                 item.getParentId().longValue() == 0).collect(Collectors.toList());
 
         if(!CollectionUtils.isEmpty(oneCategoryList)) {
             oneCategoryList.forEach(oneCategory -> {
-                List<Category> twoCategoryList = categoryList.stream().filter(item -> item.getParentId().longValue() == oneCategory.getId().longValue()).collect(Collectors.toList());
+                List<Category> twoCategoryList = allCategoryList.stream().filter(item -> item.getParentId().longValue() == oneCategory.getId().longValue()).collect(Collectors.toList());
                 oneCategory.setChildren(twoCategoryList);
 
                 if(!CollectionUtils.isEmpty(twoCategoryList)) {
                     twoCategoryList.forEach(twoCategory -> {
-                        List<Category> threeCategoryList = categoryList.stream().filter(item -> item.getParentId().longValue() == twoCategory.getId().longValue()).collect(Collectors.toList());
+                        List<Category> threeCategoryList = allCategoryList.stream().filter(item -> item.getParentId().longValue() == twoCategory.getId().longValue()).collect(Collectors.toList());
                         twoCategory.setChildren(threeCategoryList);
                     });
                 }
